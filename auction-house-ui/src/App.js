f6e13fb19eee5e7002345b18b7db0f03e41d6a65
aditisundar <aditi.sundar2000@gmail.com>
2018-05-31 16:25:27 -0400

    ui changes

--- auction-house-ui/src/App.js
+++ auction-house-ui/src/App.js
@@ -2,20 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Header extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="Header">
+        <p>
+          AUCTIONHOUSE
         </p>
       </div>
     );
   }
 }
+
+class LoginButton extends Component {
+  render() {
+    return (
+      <div className="Login-button">
+        <p className="Login-text">
+          Log in to the blockchain.
+        </p>
+      </div>
+    );
+  }
+}
+
+class LoginEntryFields extends Component {
+  render() {
+    return (
+      <div className="LoginField">
+        <p>Username:</p>
+        <input type="text" name="username"/>
+        <p>Password:</p>
+        <input type="text" name="password"/>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <div className="Login">
+          <LoginButton/>
+          <LoginEntryFields/>
+        </div>
+      </div>
+    );
+  } 
+}
+
+
+
+
+
 
 export default App;
