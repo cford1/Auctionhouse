7c02a0a71e1132bf7fa9d359df96040ab7d06cbd
aditisundar <aditi.sundar2000@gmail.com>
2018-06-01 11:42:24 -0400

    basic ui

--- auction-house-ui/src/App.js
+++ auction-house-ui/src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,11 +19,11 @@ class Header extends Component {
   }
 }
 
-class LoginButton extends Component {
+class LoginText extends Component {
   render() {
     return (
-      <div className="Login-button">
-        <p className="Login-text">
+      <div className="Login-text">
+        <p>
           Log in to the blockchain.
         </p>
       </div>
@@ -27,30 +32,106 @@ class LoginButton extends Component {
 }
 
 class LoginEntryFields extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  
+
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUserChange(event){
+    this.setState({username: event.target.value});
+  }
+  handlePassChange(event){
+    this.setState({password: event.target.value});
+  }
+  handleSubmit(event){
+    this.setState({username: "", password: ""});
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="LoginField">
         <p>Username:</p>
-        <input type="text" name="username"/>
+        <input type="text" onChange={this.handleUserChange}/>
         <p>Password:</p>
-        <input type="text" name="password"/>
+        <input type="text" onChange={this.handlePassChange} onSubmit={this.handleSubmit}/>
+        <br/>
+        <input class="Login-submit" type="submit" name="SUBMIT" onClick={this.handleSubmit}/>
+      </div>
+    );
+  }
+}
+
+class LoginPage extends Component {
+  constructor(props){
+    super(props);
+    this.state= { isAuthenticated: false };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({isAuthenticated: authenticated});
+  }
+
+  render() {
+    return (
+      <div className="LoginPage">
+        <Header/>
+        <div className="Login">
+          <LoginText/>
+          <LoginEntryFields/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class BlockchainContainer extends Component {
+  render() {
+    return (
+      <div className="BlockchainContainer">
+        <p>This is just a container</p>
+      </div>
+      
+    );
+  }
+}
+
+class AboutPage extends Component {
+  render(){
+    return (
+      <div className="About">
+        <p>This is a demo of how we can use Hyperledger Fabric to implement a blockchain.</p>
       </div>
     );
   }
 }
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-        <Header/>
-        <div className="Login">
-          <LoginButton/>
-          <LoginEntryFields/>
-        </div>
+      <div className="Page">
+      <div className="Nav">
+        <ul>
+          <li><Link to="/">Login/Register</Link></li>
+          <li><Link to="/blockchain">Blockchain</Link></li>
+          <li><Link to="/about">About</Link></li>          
+        </ul>
+
+        <Route path="/" exact component={LoginPage}/>
+        <Route path="/blockchain" exact component={BlockchainContainer}/>
+        <Route path="/about" exact component={AboutPage}/>
+      </div>
       </div>
     );
-  } 
+  }
 }
 
 
