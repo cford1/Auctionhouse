7d7dc0ddef578b578f4ea6bf20eab3744c3bf0b4
aditisundar <aditi.sundar2000@gmail.com>
2018-06-06 15:17:30 -0400

    USERS CAN REGISTER AND BE ISSUED IDENTITIES NOW

--- auction-house-ui/src/Blockchain.js
+++ auction-house-ui/src/Blockchain.js
@@ -3,159 +3,224 @@ import {
   Route,
   Link,
   BrowserRouter as Router,
-  
+
 } from 'react-router-dom';
 
+var interval = 3000;
 
 class ItemsContainer extends Component {
-    constructor() {
-      super();
-      this.state = {
-        items: []
-      };
-    }
-  
-    componentDidMount() {
-        setInterval( () => {
-            fetch('http://localhost:3000/api/org.quick.auction.Item')
-            .then(results => {
-                return results.json()
-            })
-            .then(data => this.setState({items: data}))
-        }, 5000)
-    }
-  
-    render() {
-      return (
-        <div className="ListContainer" id="Items">
-          <h1>Items</h1>
-          <table>
-            <tbody>
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/org.quick.auction.Item')
+      .then(results => {
+        return results.json()
+      })
+      .then(data => this.setState({ items: data }))
+    setInterval(() => {
+      fetch('http://localhost:3000/api/org.quick.auction.Item')
+        .then(results => {
+          return results.json()
+        })
+        .then(data => this.setState({ items: data }))
+    }, interval)
+  }
+
+  render() {
+    return (
+      <div className="ListContainer" id="Items">
+        <h1>Items</h1>
+        <table>
+          <tbody>
             <tr>
               <th>Name</th>
               <th>ID</th>
               <th>Value</th>
-  
+
             </tr>
-            
+
             {this.state.items.map(item => {
               return <tr>
-                <td key={'items-${item.name}'}>{item.name}</td> 
+                <td key={'items-${item.name}'}>{item.name}</td>
                 <td key={'items-${item.itemId}'}>{item.itemId}</td>
                 <td key={'items-${item.value}'}>${item.value}</td>
               </tr>
             })}
           </tbody>
-          </table>
-        </div>
-        
-      );
-    }
+        </table>
+      </div>
+
+    );
   }
-  
-  
-  class BuyersContainer extends Component {
-    constructor() {
-      super();
-      this.state = {
-        buyers: []
-      };
-    }
-  
-    componentDidMount() {
-        setInterval( () => {
-            fetch('http://localhost:3000/api/org.quick.auction.Buyer')
-            .then(results => {
-                return results.json()
-            })
-            .then(data => this.setState({buyers: data}))
-        }, 5000)
-    }
-  
-    render() {
-      return (
-        <div className="ListContainer" id="Buyers">
-          <h1>Buyers</h1>
-          <table>
-            <tbody>
-            <tr>
-            <th>Name</th>
-            <th>Email</th>
-            </tr>
-            
-              {this.state.buyers.map(buyer => {
-                return <tr>
-                  <td key={'buyers-${buyer.name}'}>{buyer.name}</td>
-                  <td key={'buyers-${buyer.email}'}>{buyer.email}</td>
-                </tr>
-              })}
-          </tbody>
-          </table>
-        </div>
-        
-      );
-    }
+}
+
+
+class BuyersContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      buyers: []
+    };
   }
 
-  class MarketplaceContainer extends Component {
-    constructor() {
-        super();
-        this.state = {
-          items: []
-        };
-      }
-    
-      componentDidMount() {
-        setInterval( () => {
-            fetch('http://localhost:3000/api/org.quick.auction.Item?filter=%7B%22where%22%3A%20%7B%22status%22%3A%22BEING_AUCTIONED%22%7D%7D')
-            .then(results => {
-            return results.json()
-            })
-            .then(data => this.setState({items: data}))
-        }, 5000)
-      }
-    
-      render() {
-        return (
-          <div className="ListContainer" id="Marketplace">
-            <h1>Up for Auction</h1>
-            <table>
-              <tbody>
-              <tr>
+  componentDidMount() {
+    fetch('http://localhost:3000/api/org.quick.auction.Buyer')
+      .then(results => {
+        return results.json()
+      })
+      .then(data => this.setState({ buyers: data }))
+    setInterval(() => {
+      fetch('http://localhost:3000/api/org.quick.auction.Buyer')
+        .then(results => {
+          return results.json()
+        })
+        .then(data => this.setState({ buyers: data }))
+    }, interval)
+  }
+
+  render() {
+    return (
+      <div className="ListContainer" id="Buyers">
+        <h1>Buyers</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+
+            {this.state.buyers.map(buyer => {
+              return <tr>
+                <td key={'buyers-${buyer.name}'}>{buyer.name}</td>
+                <td key={'buyers-${buyer.email}'}>{buyer.email}</td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+
+    );
+  }
+}
+
+class MarketplaceContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/org.quick.auction.Item?filter=%7B%22where%22%3A%20%7B%22status%22%3A%22BEING_AUCTIONED%22%7D%7D')
+      .then(results => {
+        return results.json()
+      })
+      .then(data => this.setState({ items: data }))
+    setInterval(() => {
+      fetch('http://localhost:3000/api/org.quick.auction.Item?filter=%7B%22where%22%3A%20%7B%22status%22%3A%22BEING_AUCTIONED%22%7D%7D')
+        .then(results => {
+          return results.json()
+        })
+        .then(data => this.setState({ items: data }))
+    }, interval)
+  }
+
+  render() {
+    return (
+      <div className="ListContainer" id="Marketplace">
+        <h1>Up for Auction</h1>
+        <table>
+          <tbody>
+            <tr>
               <th>Name</th>
               <th>ID</th>
               <th>Current Value</th>
               <th>Contact</th>
+            </tr>
+
+            {this.state.items.map(item => {
+              return <tr>
+                <td key={'items-${item.name}'}>{item.name}</td>
+                <td key={'items-${item.itemId}'}>{item.itemId}</td>
+                <td key={'items-${item.value}'}>${item.value}</td>
+                <td key={'items-${item.owner}'}>{item.owner.substring(33)}</td>
               </tr>
-              
-                {this.state.items.map(item => {
-                  return <tr>
-                    <td key={'items-${item.name}'}>{item.name}</td>
-                    <td key={'items-${item.itemId}'}>{item.itemId}</td>
-                    <td key={'items-${item.value}'}>${item.value}</td>
-                    <td key={'items-${item.owner}'}>{item.owner.substring(33)}</td>
-                  </tr>
-                })}
-            </tbody>
-            </table>
-          </div>
-          
-        );
-      }
+            })}
+          </tbody>
+        </table>
+      </div>
+
+    );
+  }
+}
+
+class TransactionContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      transactions: []
+    };
   }
 
-  class BlockchainContainer extends Component {
-    render() {
-      return (
-        <div className="BlockchainContainer">
-          <p>Registry Info</p>
-          <ItemsContainer/>
-          <BuyersContainer/>
-          <MarketplaceContainer/>
-        </div>
-        
-      );
-    }
+  componentDidMount() {
+    fetch('http://localhost:3000/api/system/historian')
+      .then(results => {
+        return results.json()
+      })
+      .then(data => this.setState({ transactions: data }))
+    setInterval(() => {
+      fetch('http://localhost:3000/api/system/historian')
+        .then(results => {
+          return results.json()
+        })
+        .then(data => this.setState({ transactions: data }))
+    }, interval)
   }
 
-  export default BlockchainContainer;
+  render() {
+    return (
+      <div className="ListContainer" id="Transactions">
+        <h1>Transaction Log</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Type</th>
+              <th>Timestamp</th>
+            </tr>
+
+            {this.state.transactions.map(transaction => {
+              return <tr>
+                <td key={'transactions-${transaction.transactionType}'}>{transaction.transactionType.substring(transaction.transactionType.lastIndexOf(".") + 1)}</td>
+                <td key={'transactions-${transaction.transactionTimestamp'}>{transaction.transactionTimestamp}</td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+
+    );
+  }
+}
+
+class BlockchainContainer extends Component {
+  render() {
+    return (
+      <div className="BlockchainContainer">
+        <p>Registry Info</p>
+        <ItemsContainer />
+        <BuyersContainer />
+        <MarketplaceContainer />
+        <TransactionContainer />
+      </div>
+
+    );
+  }
+}
+
+export default BlockchainContainer;
--- auction-house-ui/src/Registration.js
+++ auction-house-ui/src/Registration.js
@@ -3,19 +3,165 @@ import {
   Route,
   Link,
   BrowserRouter as Router,
-  
-} from 'react-router-dom';
 
-class RegistrationPage extends Component {
-    render() {
-      return (
-        <div className="RegistrationPage">
-          <div className="Register">
-         
-          </div>
-        </div>
-      );
-    }
+} from 'react-router-dom';
+import { BusinessNetworkConnection } from 'composer-client';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>
+          AUCTIONHOUSE
+          </p>
+      </div>
+    );
+  }
+}
+
+class RegEntryFields extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      accountBalance: "",
+      street: "",
+      city: "",
+      st: "",
+      zip: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+
   }
 
-  export default RegistrationPage;
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log(this);
+    console.log(JSON.stringify({
+      "$class": "org.quick.auction.AddBusiness",
+      "email": this.state.email,
+      "name": this.state.name,
+      "accountBalance": parseInt(this.state.accountBalance),
+      "address": {
+        "$class": "org.quick.auction.Address",
+        "street": this.state.street,
+        "city": this.state.city,
+        "state": this.state.st,
+        "zip": this.state.zip,
+      },
+      "status": "Buyer",
+      "transactionId": "",
+      "timestamp": (new Date()).toISOString()
+    }));
+    event.preventDefault();
+    return fetch('http://localhost:3000/api/org.quick.auction.AddBusiness', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "$class": "org.quick.auction.AddBusiness",
+        "email": this.state.email,
+        "name": this.state.name,
+        "accountBalance": this.state.accountBalance,
+        "address": {
+          "$class": "org.quick.auction.Address",
+          "street": this.state.street,
+          "city": this.state.city,
+          "state": this.state.st,
+          "zip": this.state.zip,
+        },
+        "status": "Buyer",
+        "transactionId": "",
+        "timestamp": (new Date()).toISOString()
+      }),
+    })
+      .then(() => {
+        console.log(JSON.stringify(
+          {
+            "participant": "resource:org.quick.auction.Buyer#" + this.state.email,
+            "userID": this.state.email,
+            "options": {}
+          }
+        ))
+
+
+        return fetch('http://localhost:3000/api/system/identities/issue', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(
+            {
+              "participant": "resource:org.quick.auction.Buyer#" + this.state.email,
+              "userID": this.state.email,
+              "options": {}
+            }
+          ),
+        });
+
+
+      });
+
+    /*this.setState(
+        {
+          name: "",
+          email: "",
+          accountBalance: "",
+          street: "",
+          city: "",
+          st: "",
+          zip: ""
+        });*/
+  }
+
+
+
+  render() {
+    return (
+      <div className="RegFields">
+        <div className="UserPass">
+          <p>Name:</p>
+          <input type="text" name="name" onChange={this.handleChange} />
+          <p>Email:</p>
+          <input type="text" name="email" onChange={this.handleChange} />
+          <p>Account balance:</p>
+          <input type="text" name="accountBalance" onChange={this.handleChange} />
+        </div>
+        <div className="Address">
+          <p>Street:</p>
+          <input type="text" name="street" onChange={this.handleChange} />
+          <p>City:</p>
+          <input type="text" name="city" onChange={this.handleChange} />
+          <p>State:</p>
+          <input type="text" name="st" onChange={this.handleChange} />
+          <p>Zip:</p>
+          <input type="text" name="zip" onChange={this.handleChange} />
+        </div>
+        <br />
+        <input class="Reg-submit" type="submit" name="SUBMIT" onClick={this.handleSubmit.bind(this)} />
+      </div>
+    );
+  }
+}
+
+class RegistrationPage extends Component {
+  render() {
+    return (
+      <div className="RegistrationPage">
+        <div className="Register">
+          <Header />
+          <RegEntryFields />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RegistrationPage;
